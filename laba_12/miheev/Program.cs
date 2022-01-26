f06d929c48dc55a68a365fd3ec6b3362bc482ff4
dysha <dyha035@gmail.com>
2022-01-26 09:44:35 +0500

    Version 1.0

---
+++ laba_12/miheev/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintMenu()
+{
+    Console.WriteLine("1.Ввести А\n" +
+        "2.Ввести B\n" +
+        "3.Вычислить A+B\n" +
+        "4.Вычислить A-B\n" +
+        "5.Вычислить A*B\n" +
+        "6.Вычислить A/B\n");
+}
+PrintMenu();
